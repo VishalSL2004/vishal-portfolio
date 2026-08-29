5074f68277bdf539a1fe1cf428b5acf16348ea95
VishalSL2004 <vishalbca0203@gmail.com>
2026-08-30 00:42:59 +0530

    spell changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer id="contact">
       <p className="mb-1 fw-semibold">VISHAL SL</p>
-      <p className="mb-1">Email: vishalbca0203@email.com</p>
+      <p className="mb-1">Email: vishalbca0203@gmail.com</p>
       <p className="mb-1">+91 6385202442</p>
       <p className="mb-0">© {new Date().getFullYear()} All Rights Reserved</p>
     </footer>
